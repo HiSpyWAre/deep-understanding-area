97796adb7a65d42d64d34f7ab904cb2066224f80
Nayyara Aqila <nayyaraazra468@gmail.com>
2025-09-14 14:15:32 +0700

    Update studentDatabase.java

--- studentDatabase.java
+++ studentDatabase.java
@@ -1,6 +1,8 @@
-package projectLadderJava;
+// package projectLadderJava;
 
 import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class Students {
     int ID;
@@ -14,6 +16,10 @@ class Students {
         this.Major = Major;
         this.GPA = GPA;
     }
+
+    public String toCSVString(){
+        return ID + "," + Name + "," + Major + "," + GPA;
+    }
 }
 
 public class studentDatabase {
@@ -21,28 +27,36 @@ public class studentDatabase {
         Scanner sc = new Scanner(System.in);
         List<Students> allStudent = new ArrayList<>();
         Map<String, List<Students>> data = new HashMap<>();
+
+        System.out.println("   =========================================");
+        System.out.println("   |            WELCOME TO THE             |");
+        System.out.println("   |      STUDENT DATABASE MANAGEMENT      |");
+        System.out.println("   =========================================\n");
+
         while (true) {
             System.out.println("1. Add New Student");
             System.out.println("2. Update GPA");
             System.out.println("3. Search and View Student Info");
             System.out.println("4. Class Statistics");
             System.out.println("5. Save & Exit");
+            System.out.println("Select the menu : ");
             int choice = sc.nextInt();
             sc.nextLine();
 
             switch (choice) {
                 case 1:
+                try{
                     // void addStudent();
                     System.out.println("== Add New Student ==");
                     System.out.println("(fill out the student's identity below)");
-                    System.out.println("Name = ");
+                    System.out.print("Name = ");
                     String name = sc.nextLine();
-                    System.out.println("ID = ");
+                    System.out.print("ID = ");
                     int ID = sc.nextInt();
                     sc.nextLine();
-                    System.out.println("Major = ");
+                    System.out.print("Major = ");
                     String major = sc.nextLine();
-                    System.out.println("GPA = ");
+                    System.out.print("GPA = ");
                     float GPA = sc.nextFloat();
                     sc.nextLine();
 
@@ -52,6 +66,10 @@ public class studentDatabase {
 
                     data.putIfAbsent(name, new ArrayList<>());
                     data.get(name).add(student);
+                }
+                catch(Error InputMismatchException){
+                    System.out.println("Invalid Input!");
+                }
                     break;
 
                 case 2: // update GPA
@@ -88,31 +106,27 @@ public class studentDatabase {
                     // if(!id.isEmpty())
                     for (Students e : allStudent) {
                         if (e.ID == search_id) {
-                            System.out.println("ID: " + e.ID + " | Name: " + e.Name + " | Major: " + e.Major + " | GPA: " + e.GPA);
-                            found = true;
+                            System.out.println(
+                                    "ID: " + e.ID + " | Name: " + e.Name + " | Major: " + e.Major + " | GPA: " + e.GPA);
+                            found2 = true;
                             break;
                         }
                     }
                     if (!found2) {
-                        System.out.println("GPA not found! Try again or Add data!");
+                        System.out.println("Student with that ID not found! Try again or Add data!");
                     }
                     break;
 
                 case 4:
                     System.out.println("== Class Statistics ==");
+
+                    if (allStudent.isEmpty()){
+                        System.out.println("No Student Data to show statistic. Please add student first.");
+                    }
                     // show total student number
                     System.out.println("Total Students Enrolled  = " + allStudent.size());
 
                     // gpa specific statistic
-                    int count = 0;
-                    for (Students e : allStudent) {
-                        count += e.GPA;
-                        break;
-                    }
-
-                    float mean = count / allStudent.size();
-                    System.out.println("Average Class GPA: " + mean);
-
                     float highestGPA = 0.0f;
                     float lowestGPA = 4.0f;
 
@@ -122,9 +136,11 @@ public class studentDatabase {
                     Map<String, Integer> studentPerMajor = new HashMap<>();
                     Map<String, Float> gpaPerMajor = new HashMap<>();
 
+                    float count = 0.0f;
                     for (Students e : allStudent) {
-                        mean += e.GPA;
-
+                        count += e.GPA;
+                        
+                    
                         if (e.GPA > highestGPA) {
                             highestGPA = e.GPA;
                             maxGPA = e.Name;
@@ -133,33 +149,44 @@ public class studentDatabase {
                         if (e.GPA < lowestGPA) {
                             lowestGPA = e.GPA;
                             minGPA = e.Name;
-
+                        }
+                        
                         String currentMajor = e.Major;
                         // getOrDefault is a safe way to get a value or a default (0) if it's not in the map yet.
                         studentPerMajor.put(currentMajor, studentPerMajor.getOrDefault(currentMajor, 0) + 1);
                         gpaPerMajor.put(currentMajor, gpaPerMajor.getOrDefault(currentMajor, 0.0f) + e.GPA);
-                        }
+                        
                     }
+                    
+
+                    float mean = count / allStudent.size();
+
                     System.out.println("---- GPA Information ----");
-                    System.out.printf("The highest GPA : %.2f (Achieved by : %s)", highestGPA, maxGPA);
+                    System.out.println("Average Class GPA: " + mean);
+                    System.out.printf("The highest GPA : %.2f (Achieved by : %s)"+ "\n", highestGPA, maxGPA);
                     System.out.printf("Lowest GPA:  %.2f (Achieved by: %s)\n", lowestGPA, minGPA);
 
                     // major spesific statistic
-                    System.out.println("---- Major DIstribution ----");
+                    System.out.println("---- Major Distribution ----");
 
                     for (String majorName : studentPerMajor.keySet()) {
                         int count2 = studentPerMajor.get(majorName);
                         float gpaSum = gpaPerMajor.get(majorName);
                         float majorAverageGpa = gpaSum / count2;
-                        System.out.printf("- %s: %d students (Avg. GPA: %.2f)\n", majorName, count, majorAverageGpa);
+                        System.out.printf("- %s: %d students (Avg. GPA: %.2f)\n", majorName, count2, majorAverageGpa);
                     }
                     break;
 
                 case 5:
                     try {
                         FileWriter writer = new FileWriter("database.txt");
-                        writer.write("Saving updated data.");
+                        System.out.println("Saving data....");
+                        for (Students s : allStudent){
+                            writer.write(s.toCSVString()+ "\n");
+                        }
                         writer.close();
+
+                        System.out.println("Data saved successfully");
                         System.out.println("Exiting program. Goodbye!");
                     }
 
@@ -173,12 +200,9 @@ public class studentDatabase {
                 default:
                     System.out.println("Invalid choice. Please select a valid option.");
                     break;
-
-                    
             }
 
         }
     }
 
 }
-
